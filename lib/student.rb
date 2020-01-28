cb98b3ece96234ad923e3bc3759ccb099b4923f7
ryan m <contact@ryanmeek.com>
2020-01-28 18:56:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,12 +10,12 @@ class Student
     @id = id
   end
 
-  def db_exe(sql)
+  def self.db_exe(sql)
     DB[:conn].execute(sql)
   end
 
   def self.create_table
-    db_exe(<<~SQL
+    self.db_exe(<<~SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
