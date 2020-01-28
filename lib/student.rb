a8947af06d78056f24a465deea27bb62f151e1ca
ryan m <contact@ryanmeek.com>
2020-01-28 19:14:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,4 +29,8 @@ class Student
     @id = DB[:conn].execute('SELECT last_insert_rowid() FROM students')[0][0]
   end
 
+  def update
+    DB[:conn].execute("UPDATE students SET name='#{self.name}', grade='#{self.grade}' WHERE id='#{self.id}'")
+  end
+
 end
